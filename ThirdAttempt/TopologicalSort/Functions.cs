26f3fd81d7a21600b984f3833c4b7a9358da250b
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2018-12-12 23:41:40 +0100

    Finished topological sorting
    Implementing with mutability and references (its C# but in F#)
    Started work on C# implementation because the F# version is a mess with all its references.

---
+++ ThirdAttempt/TopologicalSort/Functions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TopologicalSort
+{
+    public static class Functions
+    {
+        private static void BuildTree<ID, T>(List<Tuple<List<ID>, Node<ID, T>>> nodes, Func<ID, ID, bool> equals)
+        {
+            foreach (var node in nodes)
+            {
+                foreach (var dependencyid in node.Item1)
+                {
+                    var dependencynode = nodes.Find(x => equals(x.Item2.id, dependencyid));
+                    dependencynode.Item2.incoming_dependencies++;
+                    node.Item2.dependencies.Add(dependencynode.Item2);
+                }
+            }
+        }
+
+        private static Tuple<List<Node<ID, T>>, List<Node<ID, T>>> KahnsAlgorithm<ID, T>(List<Node<ID, T>> nodes)
+        {
+            var nodependency = nodes.Where(x => x.incoming_dependencies == 0).ToList();
+            var tosort = nodes.ToList();
+            foreach (var item in nodependency)
+            {
+                tosort.Remove(item);
+            }
+            var sorted = new List<Node<ID, T>>();
+
+            while (nodependency.Count > 0)
+            {
+                var selected = nodependency.First();
+                nodependency.RemoveAt(0);
+                sorted.Add(selected);
+                foreach (var item in selected.dependencies)
+                {
+                    item.incoming_dependencies--;
+                    if (item.incoming_dependencies == 0)
+                    {
+                        nodependency.Add(item);
+                        tosort.Remove(item);
+                    }
+                }
+            }
+            return new Tuple<List<Node<ID, T>>, List<Node<ID, T>>>(sorted, tosort);
+        }
+    }
+}
---
+++ ThirdAttempt/TopologicalSort/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopologicalSort
+{
+    class Node<ID, T>
+    {
+        public T value;
+        public ID id;
+        public List<Node<ID, T>> dependencies = new List<Node<ID, T>>();
+        public int incoming_dependencies = 0;
+
+        public Node(ID id, T value)
+        {
+            this.value = value;
+            this.id = id;
+        }
+
+        public void addDependency(Node<ID,T> node)
+        {
+            this.dependencies.Add(node);
+            node.incoming_dependencies++;
+        }
+    }
+}
