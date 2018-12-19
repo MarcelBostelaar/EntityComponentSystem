72f789b835a4e5285477838155f86817261177dc
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2018-12-19 16:00:41 +0100

    Added some more functions and an inline operator to make the code read cleaner.
    Inline operator allows syntax like:
    "top level entry" @ "child entry" @ "grandchild entry" @ RemoveRecordEntry "To remove"

--- Code/Code Generator/GenerateClasses/klad.fs
+++ Code/Code Generator/GenerateClasses/klad.fs
@@ -15,41 +15,55 @@
 
 open ParsedDataStructure
 
-let FindAndTransformSingle (name : string) (f : ParsedData -> ParsedData) (data : string*ParsedData) =
+let private FindAndTransformSingle (name : string) (f : ParsedData -> ParsedData) (data : string*ParsedData) =
     if fst data <> name
     then data
     else (name, f (snd data))
 
-let FindAndRemoveSingle (name : string) (data : string*ParsedData) =
+let private FindAndRemoveSingle (name : string) (data : string*ParsedData) =
     match name = fst data with
     | true -> None
     | false -> Some data
+
+let private FindAndRenameSingle (oldname: string) (newname: string) (data : string*ParsedData)=
+    match oldname = fst data with
+    | true -> newname, snd data
+    | false -> data
     
-let FindAndTransform (name : string) (f : ParsedData -> ParsedData) (data : (string*ParsedData) list) =
+let private FindAndTransform (name : string) (f : ParsedData -> ParsedData) (data : (string*ParsedData) list) =
     List.map (FindAndTransformSingle name f) data
 
-let FindAndRemove (name : string) (data : (string*ParsedData) list) =
+let private FindAndRemove (name : string) (data : (string*ParsedData) list) =
     List.choose (FindAndRemoveSingle name) data
+
+let private FindAndRename oldname newname data =
+    List.map (FindAndRenameSingle oldname newname) data
 
 let private MapRecord f data=
     match data with
     | ParsedData.Record x -> f x |> ParsedData.Record
     | _ -> data
     
-let FindTransformRecordEntry (name : string) (f : ParsedData -> ParsedData) (data : ParsedData) =
-    MapRecord (FindAndTransform name f) data
-
-let FindRemoveRecordEntry (name : string) (data : ParsedData) =
-    MapRecord (FindAndRemove name) data
-
-let AddData (name : string) (data) (record : (string * ParsedData) list) =
+let private AddData (name : string) (data) (record : (string * ParsedData) list) =
     match List.where (fun x -> fst x = name) record |> List.length with
     | 0 -> (name, data) :: record
     | _ -> raise (new System.Exception("This transformation ought not to be possible, tried to add value to a record where an entry with that name already exists"))
+    
+let TransformRecordEntry (name : string) (f : ParsedData -> ParsedData) (data : ParsedData) =
+    MapRecord (FindAndTransform name f) data
+
+let RemoveRecordEntry (name : string) (data : ParsedData) =
+    MapRecord (FindAndRemove name) data
+
+let RenameRecordEntry oldname newname data =
+    MapRecord (FindAndRename oldname newname) data
 
 let AddRecordEntry name value_to_add data=
     MapRecord (AddData name value_to_add) data
+    
+let (@) entryname func = TransformRecordEntry entryname func
 
-let test = FindRemoveRecordEntry "removethis" |> FindTransformRecordEntry "nested" |> FindTransformRecordEntry "Point"
+let test = "Point" @ "nested" @ RemoveRecordEntry "removethis"
 
-//FindTransformRecordEntry "point" (FindTransformRecordEntry "nested" (FindRemoveRecordEntry "removethis"))
+let test2 = RenameRecordEntry "old" "new" |> TransformRecordEntry "nested" |> TransformRecordEntry "Point"
+let testt2 = "Point" @ "nested" @ RenameRecordEntry "old" "new"
