8ec5f95ee81d13a4c2038c9019259205790c3b27
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2019-01-27 12:46:54 +0100

    Working on bigexponent

--- Code/ParserBase/ParsedDataStructure.fs
+++ Code/ParserBase/ParsedDataStructure.fs
@@ -1,10 +1,21 @@
 ﻿module ParsedDataStructure
 
+open System.Numerics
 open System
+
+type BigExponent = {value: bigint; exponent: bigint; num_base: bigint} with
+    member this.Simplify = 
+        let mutable baseDivisions = 0
+        let mutable value = this.value
+        while BigInteger.GreatestCommonDivisor(value, this.num_base) = this.num_base do
+            baseDivisions <- baseDivisions + 1
+            value <- value/this.num_base
+        {value = value; exponent= this.exponent + bigint baseDivisions; num_base= this.num_base}
+    member this.IsInt = this.Simplify.exponent >= 0I
 
 type ParsedData =
     | String of string
-    | Float of float
+    | Number of BigExponent
     | List of ParsedData list
     | Boolean of bool
     | Record of (string*ParsedData) list
