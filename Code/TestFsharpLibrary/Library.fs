c7ce4fdeb303259e8aa16640172afcec1290977c
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2019-02-18 16:42:18 +0100

    Some examplecode

---
+++ Code/TestFsharpLibrary/Library.fs
@@ -0,0 +1,20 @@
+﻿module TestFsharpLibrary
+
+open TestLibrary
+open MatchBuild
+open ParsedDataStructure
+open JsonParsedDataConverter
+
+
+let matchint name i = Ok 1
+let matchfloat name i = Ok 1.1f
+let matchstring name i = Ok "Hoi"
+
+let serializeint (i : int) = ParsedData.Number {value = bigint i; exponent= 0I; num_base= 10I}
+let serializefloat (i : float32) = naivefloattoexponent (float i) |> ParsedData.Number 
+let serializestring (i : string) = ParsedData.String i
+
+let constructorwrapper a b c= ExampleClass (a,b,c)
+let builder = MatchBuild3 constructorwrapper (matchint "one") (matchfloat "two") (matchstring "three")
+ 
+let deconstructer (i : ExampleClass) = ["one", serializeint i.one; "two", serializefloat i.two; "three", serializestring i.three] |> Record
---
+++ Code/TestLibrary/ExampleClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class ExampleClass
+    {
+        public int one;
+        public float two;
+        public string three;
+        public ExampleClass(int one, float two, string three)
+        {
+            this.one = one;
+            this.two = two;
+            this.three = three;
+        }
+    }
+}
