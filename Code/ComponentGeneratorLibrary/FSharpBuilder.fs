215e332e6eba5b15fb1248a24650c666654c70f6
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2019-02-19 15:03:55 +0100

    Fixed mistake where fieldname and typename were confused

--- Code/ComponentGeneratorLibrary/FSharpBuilder.fs
+++ Code/ComponentGeneratorLibrary/FSharpBuilder.fs
@@ -11,16 +11,15 @@ open ParsedDataStructure
 let constructorwrappername = "constructorwrapper"
 let functionspacer = "\n\n"
 
-type DeSerializeInfo = {name: string; serializer: string; deserializer: string; modulename: string}
+type DeSerializeInfo = {TypeName: string; serializer: string; deserializer: string; modulename: string}
 
-let private BuildConstructorWrapper (data : Filenamepair<DeSerializeInfo list>) = 
-    let fieldnames = List.map (fun x -> x.name) data.data
-    String.Format("let {3} {0} = {1} ({2})", String.concat " " fieldnames, data.filename, String.concat ", " fieldnames, constructorwrappername)
+let private BuildConstructorWrapper (data : Filenamepair<string list>) = 
+    String.Format("let {3} {0} = {1} ({2})", String.concat " " data.data, data.filename, String.concat ", " data.data, constructorwrappername)
 
-let private BuildModuleIncludes (data : Filenamepair<DeSerializeInfo list>) =
-    List.map (fun x -> x.modulename) data.data |> set |> seq |> Seq.map (fun x -> "open " + x) |> String.concat "\n"
+let private BuildModuleIncludes (data : Filenamepair<(string*DeSerializeInfo) list>) =
+    List.map (fun x -> (snd x).modulename) data.data |> set |> seq |> Seq.map (fun x -> "open " + x) |> String.concat "\n"
 
-let private BuildDeserializer (data : Filenamepair<DeSerializeInfo list>)=
+let private BuildDeserializer (data : Filenamepair<(string * DeSerializeInfo) list>)=
     let length = data.data.Length
     let buildername = data.filename + "Deserializer"
     let matchbuildstring = GenerateMatchBuild length
@@ -28,34 +27,34 @@ let private BuildDeserializer (data : Filenamepair<DeSerializeInfo list>)=
         "let {3} = MatchBuild{0} {1} {2}",
         length,
         constructorwrappername,
-        List.map (fun (x : DeSerializeInfo) -> "(" + x.deserializer + " \"" + x.name + "\")") data.data |> String.concat " ",
+        List.map (fun x -> "(" + (snd x).deserializer + " \"" + fst x + "\")") data.data |> String.concat " ",
         buildername
     )
     matchbuildstring, builder, buildername
 
-let private BuildSerialiser (data: Filenamepair<DeSerializeInfo list>)=
+let private BuildSerialiser (data: Filenamepair<(string * DeSerializeInfo) list>)=
     let serializername = data.filename + "Serializer"
     let functionstring = String.Format("let {0} (i : {1}) = [{2}] |> Record",
         serializername,
         data.filename,
-        List.map (fun (x : DeSerializeInfo) -> String.Format("\"{0}\", {1} i.{2}", x.name, x.serializer, x.name)) data.data |> String.concat ", "
+        List.map (fun x -> String.Format("\"{0}\", {1} i.{2}", fst x, (snd x).serializer, fst x)) data.data |> String.concat ", "
     )
     functionstring, serializername
 
-let private UnboundFullBuilder (data : Filenamepair<DeSerializeInfo list>)=
+let private UnboundFullBuilder (data : Filenamepair<(string * DeSerializeInfo) list>)=
     let topmodule = "module " + data.filename;
     let matchbuildstring, builderstring, deserializername = BuildDeserializer data
     let buildincludestring = BuildModuleIncludes data
-    let constructorwrapperstring = BuildConstructorWrapper data
+    let constructorwrapperstring = BuildConstructorWrapper (FilenamepairMap (List.map fst) data)
     let serializerstring, serializername = BuildSerialiser data
     let fsharpfilestring = [topmodule; buildincludestring; matchbuildstring;constructorwrapperstring;builderstring;serializerstring] |> String.concat functionspacer
-    fsharpfilestring, {name= data.filename; serializer= serializername; deserializer= deserializername; modulename= data.filename}
+    fsharpfilestring, {TypeName= data.filename; serializer= serializername; deserializer= deserializername; modulename= data.filename}
 
 let private BuildSingleFile data = FilenamepairBind UnboundFullBuilder data
 
 let private replacefield (existingtypes : DeSerializeInfo list) (data : ParsedField)  =
-    match List.tryFind (fun (x : DeSerializeInfo) ->x.name = data.typename) existingtypes with
-    | Some x -> Ok x
+    match List.tryFind (fun (x : DeSerializeInfo) ->x.TypeName = data.typename) existingtypes with
+    | Some x -> Ok (data.name, x)
     | None -> Error data
 
 let private replacefields (existingtypes : DeSerializeInfo list) (data: ParsedField list) =
